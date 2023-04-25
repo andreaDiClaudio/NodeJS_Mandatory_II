440828885cdb46ef83f9bad3bc91535220ca7171
andreaDiClaudio <andreadicla@gmail.com>
2023-04-25 10:12:53 +0200

    Started working on the client. Initialised Svelte and be able to route to signup and login

---
+++ Client/Mandatory_II/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+ 	import { Router, Link, Route } from "svelte-navigator";
+  import Login from "./pages/login/Login.svelte";
+  import Signup from "./pages/signup/Signup.svelte";
+</script>
+
+<Router>
+
+  <Route path="/login">
+    <Login />
+  </Route>
+
+  <Route path="/signup">
+    <Signup />
+  </Route>
+</Router>
